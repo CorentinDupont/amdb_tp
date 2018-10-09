ba28d9762263f94d4b49e6e2a3cbf0731c879c30
CorentinDupont <dupont.corentin44@gmail.com>
2018-10-09 10:27:44 +0200

    fix

--- amdb_tp/src/pages/home/home.ts
+++ amdb_tp/src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { ApiProvider } from './../../providers/api/api';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -7,8 +8,60 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  data:any;
+  movies: string[];
+  errorMessage: string;
+  page = 1;
+  perPage = 0;
+  totalData = 0;
+  totalPage = 0;
 
+  constructor(public navCtrl: NavController, public api :ApiProvider) {
+    console.log("init home page")
+    this.getMovies();
+    console.log("movies", this.movies);
+    console.log("page", this.page);
+    console.log("totalData", this.totalData);
+    console.log("totalPage", this.totalPage);
+  }
+
+  getMovies() {
+    this.api.getMovies(this.page)
+       .subscribe(
+         res => {
+           console.log(res);
+           this.data = res;
+           this.movies = this.data.Search;
+           this.perPage = 10;
+           this.totalData = this.data.totalResults;
+           this.totalPage = 20;
+         },
+         error =>  this.errorMessage = <any>error);
+  }
+
+  doInfinite(infiniteScroll) {
+    this.page = this.page+1;
+    setTimeout(() => {
+      this.api.getMovies(this.page)
+         .subscribe(
+           res => {
+            this.data = res;
+            this.perPage = 10;
+            this.totalData = this.data.totalResults;
+            this.totalPage = 20;
+            for(let i=0; i<this.data.Search.legnth; i++) {
+              this.movies.push(this.data.Search[i]);
+            }
+           },
+           error =>  this.errorMessage = <any>error);
+  
+      console.log('Async operation has ended');
+      infiniteScroll.complete();
+    }, 1000);
+  }
+
+  movieToString(movie){
+    return JSON.stringify(movie)
   }
 
 }
---
+++ amdb_tp/src/providers/api/api.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the ApiProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ApiProvider {
+
+  private apiUrl = 'http://www.omdbapi.com/?';
+  private apiKey = '&apikey=feb2a5b1'
+
+  constructor(public http: Http) {
+    
+  }
+
+  getMovies(page): Observable<string[]> {
+    return this.http.get(this.apiUrl+"s=ass&page="+page+this.apiKey)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body || { };
+  }
+
+  private handleError (error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+
+}
